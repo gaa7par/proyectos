e45608171214019646f0afae542b578d7b013c16
gaa7par <gaa7par@gmail.com>
2016-06-08 14:55:51 +0200

    Add tests of User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(first_name: "Foo", last_name: "Bar", email: "email@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "first_name should be present" do
+    @user.first_name = ""
+    assert_not @user.valid?
+  end
+
+  test "last_name should be present" do
+    @user.last_name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 41
+    assert_not @user.valid?
+  end
 end
