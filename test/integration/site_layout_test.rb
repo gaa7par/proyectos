75d021a19b7dd23fdae9604c6165b93f2b07de3c
gaa7par <gaa7par@gmail.com>
2016-06-08 18:06:06 +0200

    Extend site_layout integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,13 +5,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'welcome/index'
     assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", projects_path
 
     get users_path
     assert_template 'users/index'
     assert_select "a[href=?]", new_user_path
+    assert_select "a[href=?]", root_path
 
     get new_user_path
     assert_template 'users/new'
     assert_select "a[href=?]", users_path
+
+    get projects_path
+    assert_template 'projects/index'
+    assert_select "a[href=?]", new_project_path
+    assert_select "a[href=?]", root_path
+
+    get new_project_path
+    assert_template 'projects/new'
+    assert_select "a[href=?]", projects_path
   end
 end
